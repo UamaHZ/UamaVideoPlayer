727f9625b63d5ad09198faa7f71e08ee78952570
jinfeng <jinfeng@uama.com.cn>
2018-01-12 15:25:08 +0800

    修改测试配置

--- app/src/androidTest/java/com/uama/vieoplayer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/uama/vieoplayer/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.xiao.nicevieoplayer", appContext.getPackageName());
+        assertEquals("com.uama.vieoplayer", appContext.getPackageName());
     }
 }
